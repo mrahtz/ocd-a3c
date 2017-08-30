2d7b022b9578fe606e617519057d33c8fe7255c0
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-08-30 15:47:56 +0200

    Calculate entropy properly

--- network.py
+++ network.py
@@ -80,7 +80,7 @@ def create_network(scope):
 
             # We want to maximise entropy, which is the same as
             # minimising negative entropy
-            policy_loss -= tf.reduce_sum(BETA * entropy(a_softmax))
+            policy_loss -= tf.reduce_sum(BETA * entropy(a_logits))
 
             value_loss = advantage ** 2
             value_loss = tf.reduce_sum(value_loss)
