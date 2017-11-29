98ee821d05f926075417c8ecf80f5a7928dfc95d
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-11-29 12:41:06 +0100

    Add summaries for 8 workers

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..3}; do
-	python -u ./run.py 4 $i &> ${i}.log &
+for i in {1..7}; do
+	python -u ./run.py 8 $i &> ${i}.log &
 done
-python -u ./run.py 4 0 &> 0.log
+python -u ./run.py 8 0 &> 0.log
