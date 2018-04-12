3bab1ba025dda20137ac0a9e05805857e67c4f9d
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-12 20:08:17 +0200

    Tidy up train_ops_test

--- train_ops_test.py
+++ train_ops_test.py
@@ -5,27 +5,18 @@ import unittest
 import numpy as np
 from train_ops import create_train_ops
 
-grad_bufs = None
-sess = None
-
-
-def assert_grad_bufs_zero():
-    global grad_bufs
-    for buf in grad_bufs.items():
-        val = sess.run(buf)[0]
-        np.testing.assert_equal(val, np.array([0., 0.]))
-
 
 class TestTrainOps(unittest.TestCase):
 
-    def testUnusedVariables(self):
+    def setUp(self):
+        tf.reset_default_graph()
+        self.sess = tf.Session()
+
+    def test_unused_variables(self):
         """
         Test whether everything behaves correctly if we have a trainable
         variable which isn't relevant to the loss function
         """
-        tf.reset_default_graph()
-        sess = tf.Session()
-
         scopes = ['update_scope', 'apply_scope']
         ops = {}
         for scope in scopes:
@@ -35,37 +26,39 @@ class TestTrainOps(unittest.TestCase):
                 ops[scope]['w2'] = tf.Variable(10.0)
                 ops[scope]['loss'] = ops[scope]['w1']
 
-        o = tf.train.GradientDescentOptimizer(learning_rate=1)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=1)
 
-        update_ops, apply_ops, zero_ops = \
-            create_train_ops(ops['update_scope']['loss'],
-                             o,
-                             'update_scope',
-                             'apply_scope')
+        update_ops, apply_ops, zero_ops, _ = create_train_ops(
+            ops['update_scope']['loss'],
+            optimizer,
+            'update_scope',
+            'apply_scope')
 
-        sess.run(tf.global_variables_initializer())
-        sess.run(update_ops)
-        sess.run(update_ops)
-        sess.run(update_ops)
-        sess.run(apply_ops)
+        self.sess.run(tf.global_variables_initializer())
+        self.sess.run(update_ops)
+        self.sess.run(update_ops)
+        self.sess.run(update_ops)
+        self.sess.run(apply_ops)
 
-        actual = sess.run(ops['update_scope']['w1'])
-        expected = 10.0
-        np.testing.assert_equal(actual, expected)
+        # Check that variables in update_scope haven't been touched
+        w1, w2 = self.sess.run([ops['update_scope']['w1'],
+                                ops['update_scope']['w2']])
+        np.testing.assert_equal(w1, 10.0)
+        np.testing.assert_equal(w2, 10.0)
 
-        actual = sess.run(ops['update_scope']['w2'])
-        expected = 10.0
-        np.testing.assert_equal(actual, expected)
+        # Check that w1 has been updated, but that w2 hasn't
+        # (since it wasn't relevant to the loss)
+        w1, w2 = self.sess.run([ops['apply_scope']['w1'],
+                                ops['apply_scope']['w2']])
+        np.testing.assert_equal(w1, 7.0)
+        np.testing.assert_equal(w2, 10.0)
 
-        actual = sess.run(ops['apply_scope']['w1'])
-        expected = 7.0
-        np.testing.assert_equal(actual, expected)
+    def assert_grad_bufs_zero(self, grad_bufs):
+        for buf in grad_bufs.values():
+            val = self.sess.run(buf)[0]
+            np.testing.assert_equal(val, np.array([0., 0.]))
 
-        actual = sess.run(ops['apply_scope']['w2'])
-        expected = 10.0
-        np.testing.assert_equal(actual, expected)
-
-    def test(self):
+    def test_full_run(self):
         global grad_bufs
         global sess
 
@@ -75,13 +68,11 @@ class TestTrainOps(unittest.TestCase):
 
         scopes = ['update_scope', 'apply_scope']
 
-        tf.reset_default_graph()
-        sess = tf.Session()
-
-        input = tf.placeholder(tf.float32, [None, 2])
-
+        w2_mult = tf.placeholder(tf.float32, [None, 2])
         vars = {}
         losses = {}
+        # Create two variables in two scopes
+        # Each variable is a vector with two elements
         for scope in scopes:
             with tf.variable_scope(scope):
                 w1 = tf.Variable(inits['w1'], name='w1')
@@ -89,115 +80,102 @@ class TestTrainOps(unittest.TestCase):
                 # NB reduce_sum is necessary to ensure that the gradients
                 # accumulated for multiple examples in a batch are the same as
                 # if the examples were presented in individual batches
-                losses[scope] = tf.reduce_sum(w1 + input * w2, axis=-1)
+                losses[scope] = tf.reduce_sum(w1 + w2_mult * w2, axis=-1)
                 vars[scope] = {'w1': w1, 'w2': w2}
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=1)
 
-        o = tf.train.GradientDescentOptimizer(learning_rate=1)
+        # Check that no extra trainable variables are introduced
+        # We have two variables, two scopes, for a total of 4 trainable
+        # variables
+        assert len(tf.trainable_variables()) == 4
+        update_ops, apply_ops, zero_ops, grad_bufs = create_train_ops(
+            losses['update_scope'], optimizer, 'update_scope', 'apply_scope')
+        assert len(tf.trainable_variables()) == 4
 
-        """
-        Check that no extra trainable variables have been introduced.
-        """
-        # two variables, two scopes, for a total of 4 trainable variables
-        assert(len(tf.trainable_variables()) == 4)
+        self.sess.run(tf.global_variables_initializer())
 
-        update_ops, apply_ops, zero_ops = create_train_ops(losses['update_scope'], o, 'update_scope', 'apply_scope')
+        # Check that the gradient buffers start out zero
+        self.assert_grad_bufs_zero(grad_bufs)
 
-        assert(len(tf.trainable_variables()) == 4)
+        # Add one set of gradients to the gradient buffers
 
-        sess.run(tf.global_variables_initializer())
+        # Loss in element 0 of batch will be w1 + [1, 1] . w2
+        # Loss in element 1 of batch will be w1 + [2, 2] . w2
+        self.sess.run(update_ops, feed_dict={w2_mult: [[1, 1],
+                                                       [2, 2]]})
 
-        grad_bufs = {v.name: v for v in tf.global_variables() if 'grad_buf' in v.name}
-
-        """
-        Check that the gradient buffers start out zero.
-        """
-        assert_grad_bufs_zero()
-
-        # so the first loss term looks like w1 + 1 * w2
-        # and the second term looks like w1 + 2 * w2
-        sess.run(update_ops, feed_dict={input: [[1, 1],
-                                                [2, 2]]})
-
-        """
-        Confirm that no changes have taken place to the trainable
-        variables yet in either scope.
-        """
+        # Confirm that no changes have taken place to the trainable
+        # variables yet in either scope
         for scope in scopes:
             for var_name, var in vars[scope].items():
-                val = sess.run(var)
+                val = self.sess.run(var)
                 np.testing.assert_equal(val, inits[var_name])
 
-        """
-        Confirm that the gradient buffers look reasonable.
-        """
-        for buf_name, buf in grad_bufs.items():
-            actual = sess.run(buf)
-            # first loss term was w1 + 1 * w2
-            # second was w1 + 2 * w2
-            # first loss term contribution:
-            # derivative wrt to each element of both vectors should be 1
-            # second loss term contribution:
-            # derivative wrt w1 should be 1; derivative wrt w2 should be 2
-            if 'w1' in buf_name:
-                expected = np.array([1., 1.]) + np.array([1., 1.])
-            elif 'w2' in buf_name:
-                expected = np.array([1., 1.]) + np.array([2., 2.])
-            np.testing.assert_equal(actual, expected)
+        # Confirm that the gradient buffers have the right values.
+        # Loss term in element 0 of the batch was w1 + [1, 1] . w2.
+        # Derivative of loss wrt to each element of both w1 and w2 should be 1.
+        # In element 1, was w1 + [2, 2] . w2.
+        # Derivative should be 1 for each element of w1,
+        # and 2 for each element of w2.
+        # Total derivatives accumulated for each element of w1: 1 + 1 = 2.
+        actual = self.sess.run(grad_bufs['w1'])
+        expected = np.array([1. + 1.,
+                             1. + 1.])
+        np.testing.assert_equal(actual, expected)
+        # Total derivatives accumulated for each element of w2: 1 + 2 = 3.
+        actual = self.sess.run(grad_bufs['w2'])
+        expected = np.array([1. + 2.,
+                             1. + 2.])
+        np.testing.assert_equal(actual, expected)
 
-        # loss will be e.g. w1 + [3, 4] * w2
-        sess.run(update_ops, feed_dict={input: [[3, 4],
-                                                [5, 6]]})
+        # Add another set of gradients to the gradient buffers
 
-        """
-        Confirm that the gradient buffers still look reasonable.
-        """
-        for buf_name, buf in grad_bufs.items():
-            actual = sess.run(buf)
-            if 'w1' in buf_name:
-                expected = np.array([1., 1.]) + np.array([1., 1.]) + \
-                           np.array([1., 1.]) + np.array([1., 1.])
-            elif 'w2' in buf_name:
-                expected = np.array([1., 1.]) + np.array([2., 2.]) + \
-                           np.array([3., 4.]) + np.array([5., 6.])
-            np.testing.assert_equal(actual, expected)
+        # Losses will be w1 + [3, 4] . w1,
+        #                w1 + [5, 6] . w2
+        self.sess.run(update_ops, feed_dict={w2_mult: [[3, 4],
+                                                       [5, 6]]})
 
-        sess.run(apply_ops)
+        # Confirm that the gradient buffers are still right.
+        actual = self.sess.run(grad_bufs['w1'])
+        expected = np.array([1. + 1. + 1. + 1.,
+                             1. + 1. + 1. + 1.])
+        np.testing.assert_equal(actual, expected)
+        actual = self.sess.run(grad_bufs['w2'])
+        expected = np.array([1. + 2. + 3. + 5.,
+                             1. + 2. + 4. + 6.])
+        np.testing.assert_equal(actual, expected)
 
-        """
-        Confirm that no changes have been made to the variables in update_scope.
-        """
-        for var_name, var in vars['update_scope'].items():
-            actual = sess.run(var)
-            if 'w1' in var_name:
-                expected = inits['w1']
-            elif 'w2' in var_name:
-                expected = inits['w2']
-            np.testing.assert_equal(actual, expected)
+        # Apply the gradient buffers
 
-        """
-        Confirm that changes _have_ been made to the variables in apply_scope.
-        """
-        for var_name, var in vars['apply_scope'].items():
-            actual = sess.run(var)
-            # w1 started off as [10, 20];
-            # gradient wrt w1 was 1 on each step,
-            # and we went for 4 steps with step size of 1
-            if 'w1' in var_name:
-                expected = [10 - 1. - 1. - 1. - 1.,
-                            20 - 1. - 1. - 1. - 1.]
-            # w2 started off as [5, 10]
-            # gradients were [1, 1], [2, 2], [3, 4], and [5, 6]
-            elif 'w2' in var_name:
-                expected = [5.  - 1. - 2. - 3. - 5.,
-                            10. - 1. - 2. - 4. - 6.]
-            np.testing.assert_equal(actual, expected)
+        self.sess.run(apply_ops)
 
-        sess.run(zero_ops)
+        # Confirm that no changes have been made to the variables in
+        # update_scope
+        actual = self.sess.run(vars['update_scope']['w1'])
+        expected = inits['w1']
+        np.testing.assert_equal(actual, expected)
+        actual = self.sess.run(vars['update_scope']['w2'])
+        expected = inits['w2']
+        np.testing.assert_equal(actual, expected)
 
-        """
-        Check that gradient buffers have been zeroed.
-        """
-        assert_grad_bufs_zero()
+        # Confirm that changes _have_ been made to the variables in apply_scope.
+        actual = self.sess.run(vars['apply_scope']['w1'])
+        # w1 started off as [10, 20];
+        # gradient wrt w1 was 1 on each step,
+        # and we went for 4 steps with step size of 1
+        expected = [10 - 1. - 1. - 1. - 1.,
+                    20 - 1. - 1. - 1. - 1.]
+        np.testing.assert_equal(actual, expected)
+        actual = self.sess.run(vars['apply_scope']['w2'])
+        # w2 started off as [5, 10]
+        # gradients were [1, 1], [2, 2], [3, 4], and [5, 6]
+        expected = [5. - 1. - 2. - 3. - 5.,
+                    10. - 1. - 2. - 4. - 6.]
+        np.testing.assert_equal(actual, expected)
+
+        # Check that zeroing the gradient buffers works.
+        self.sess.run(zero_ops)
+        self.assert_grad_bufs_zero(grad_bufs)
 
 
 if __name__ == '__main__':
