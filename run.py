10f7463a96e50652252e783af291740780e5a80e
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-13 19:53:56 +0200

    Simplify run script

--- run.py
+++ run.py
@@ -8,37 +8,31 @@ import tensorflow as tf
 
 from network import create_network
 from worker import Worker
+from multiprocessing import Process
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # filter out INFO messages
 
 
-def worker(env_id, i, ckpt_freq, load_ckpt_file, render):
-    """
-    Set up a single worker.
-
-    I'm still not 100% about how Distributed TensorFlow works, but as I
-    understand it we do "between-graph replication": each worker has a separate
-    graph, with the global set of parameters shared between all workers (pinned
-    to worker 0).
-    """
-    dirname = 'summaries/%d_worker%d' % (int(time.time()), i)
+def start_worker(env_id, worker_n, ckpt_freq, load_ckpt_file, render):
+    dirname = 'summaries/%d_worker%d' % (int(time.time()), worker_n)
     os.makedirs(dirname)
     summary_writer = tf.summary.FileWriter(dirname, flush_secs=1)
 
-    tf.reset_default_graph()
-    server = tf.train.Server(cluster, job_name="worker", task_index=i)
+    server = tf.train.Server(cluster, job_name="worker", task_index=worker_n)
     sess = tf.Session(server.target)
 
     with tf.device("/job:worker/task:0"):
         create_network('global')
-    with tf.device("/job:worker/task:%d" % i):
-        w = Worker(sess, i, env_id, summary_writer)
+    with tf.device("/job:worker/task:%d" % worker_n):
+        w = Worker(sess, worker_n, env_id, summary_writer)
         if render:
             w.render = True
 
-    if i == 0:
+    if worker_n == 0:
         saver = tf.train.Saver()
         checkpoint_file = os.path.join('checkpoints', 'network.ckpt')
 
-    print("Waiting for cluster cluster connection...")
+    print("Waiting for cluster connection...")
     sess.run(tf.global_variables_initializer())
 
     if load_ckpt_file is not None:
@@ -55,16 +49,15 @@ def worker(env_id, i, ckpt_freq, load_ckpt_file, render):
         if done:
             w.reset_env()
         step += 1
-        if (i == 0) and (step % ckpt_freq == 0):
+        if (worker_n == 0) and (step % ckpt_freq == 0):
             print("Saving checkpoint at step %d..." % step, end='', flush=True)
             saver.save(sess, checkpoint_file)
             print("done!")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("n_workers", type=int)
-parser.add_argument("worker_n", type=int)
 parser.add_argument("env_id")
+parser.add_argument("n_workers", type=int)
 parser.add_argument("--port_start", type=int, default=2200)
 parser.add_argument("--ckpt_freq", type=int, default=5)
 parser.add_argument("--load_ckpt")
@@ -78,8 +71,24 @@ cluster_dict = {}
 workers = []
 for i in range(args.n_workers):
     port = args.port_start + i
-    workers.append("localhost:%d" % port)
+    workers.append("localhost:{}".format(port))
 cluster_dict["worker"] = workers
 cluster = tf.train.ClusterSpec(cluster_dict)
 
-worker(args.env_id, args.worker_n, args.ckpt_freq, args.load_ckpt, args.render)
+
+def start_worker_process(worker_n):
+    print("Starting worker", worker_n)
+    start_worker(args.env_id,
+                 worker_n,
+                 args.ckpt_freq,
+                 args.load_ckpt,
+                 args.render)
+
+
+worker_processes = []
+for worker_n in range(args.n_workers):
+    p = Process(target=start_worker_process, args=(worker_n,))
+    p.start()
+    worker_processes.append(p)
+for p in worker_processes:
+    p.join()
