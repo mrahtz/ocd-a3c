9627a4449690c880252339a89160072fce29107a
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-16 09:35:15 +0200

    Implement memory logging

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import argparse
 import os
+import os.path as osp
 import time
 from multiprocessing import Process
 
@@ -10,13 +11,14 @@ import tensorflow as tf
 import time
 
 from network import create_network
-from utils import get_port_range
+from utils import get_port_range, profile_memory, get_git_rev
 from worker import Worker
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # filter out INFO messages
 
-def run_worker(env_id, worker_n, n_steps, ckpt_freq, load_ckpt_file, render):
-    dirname = 'summaries/%d_worker%d' % (int(time.time()), worker_n)
+def run_worker(env_id, worker_n, n_steps, ckpt_freq, load_ckpt_file, render,
+               log_dir):
+    dirname = osp.join(log_dir, "worker_{}".format(worker_n))
     os.makedirs(dirname)
     summary_writer = tf.summary.FileWriter(dirname, flush_secs=1)
 
@@ -59,15 +61,25 @@ def run_worker(env_id, worker_n, n_steps, ckpt_freq, load_ckpt_file, render):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("env_id")
-parser.add_argument("--n_steps", type=int)
+parser.add_argument("--n_steps", type=int, default=10)
 parser.add_argument("--n_workers", type=int, default=16)
 parser.add_argument("--ckpt_freq", type=int, default=5)
 parser.add_argument("--load_ckpt")
 parser.add_argument("--render", action='store_true')
+seconds_since_epoch = str(int(time.time()))
+parser.add_argument('--run_name', default=seconds_since_epoch)
 args = parser.parse_args()
 
+git_rev = get_git_rev()
+run_name = args.run_name + '_' + git_rev
+log_dir = osp.join('runs', run_name)
+if osp.exists(log_dir):
+    raise Exception("Log directory '%s' already exists" % log_dir)
+os.makedirs(log_dir)
+
 if "MovingDot" in args.env_id:
-    pass
+    import gym_moving_dot
+    gym_moving_dot  # TODO prevent PyCharm from removing the import
 
 cluster_dict = {}
 ports = get_port_range(start_port=2200, n_ports=args.n_workers)
@@ -82,13 +94,28 @@ def start_worker_process(worker_n):
                args.n_steps,
                args.ckpt_freq,
                args.load_ckpt,
-               args.render)
+               args.render,
+               log_dir)
 
 
 worker_processes = []
+memory_profiler_processes = []
 for worker_n in range(args.n_workers):
-    p = Process(target=start_worker_process, args=(worker_n,))
+    p = Process(target=start_worker_process, args=(worker_n,), daemon=True)
     p.start()
     worker_processes.append(p)
+
+    # TODO: remove
+    mem_log = osp.join(log_dir, "worker_{}_memory.log".format(worker_n))
+    mp = profile_memory(mem_log, p.pid)
+    memory_profiler_processes.append(mp)
+
+
 for p in worker_processes:
+    print("Joining", p)
     p.join()
+print("Joined")
+for p in memory_profiler_processes:
+    print("Terminating mp", p)
+    p.terminate()
+print("Terminated")
