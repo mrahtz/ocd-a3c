64299e1b37249ec2c39f0d0b567dfec5887ba00d
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-24 20:34:59 +0200

    Add show_observations.py

---
+++ show_observations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Want to check exactly what the observations going into your network look like?
+Add a line like this to your graph:
+
+    obs = tf.Print(obs, [obs],
+                   message='Observations: ',
+                   summarize=2147483647)  # max int32
+
+Then run this script on the output of your run to view the saved observations.
+"""
+
+import argparse
+import re
+
+from pylab import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('log_file', type=argparse.FileType('r'))
+args = parser.parse_args()
+
+for line in args.log_file:
+    prefix = 'Observations: '
+    if not line.startswith(prefix):
+        continue
+    line = line[len(prefix):]
+    line = re.sub('([0-9]) ', '\\1, ', line)
+    line = re.sub('\]', '], ', line)
+    obs = np.array(eval(line)[0])
+    print("Found observation with shape", obs.shape)
+    if obs.shape == (1, 84, 84, 4):
+        obs = obs[0]
+        obs = np.concatenate((obs[..., 0],
+                              obs[..., 1],
+                              obs[..., 2],
+                              obs[..., 3]),
+                             axis=1)
+    imshow(obs, cmap='gray')
+    show()
