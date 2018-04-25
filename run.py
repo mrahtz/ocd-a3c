60eaf8c83d38901571bc790f60b43ff696986238
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-25 18:58:53 +0200

    Save checkpoints by time rather than steps

--- run.py
+++ run.py
@@ -11,13 +11,14 @@ import tensorflow as tf
 
 import preprocessing
 from network import create_network
-from utils import get_port_range, MemoryProfiler, get_git_rev
+from utils import get_port_range, MemoryProfiler, get_git_rev, Timer
 from worker import Worker
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # filter out INFO messages
 
 
-def run_worker(env_id, preprocess_wrapper, seed, worker_n, n_steps_to_run, ckpt_freq,
+def run_worker(env_id, preprocess_wrapper, seed, worker_n, n_steps_to_run,
+               ckpt_timer,
                load_ckpt_file, render, log_dir):
     mem_log = osp.join(log_dir, "worker_{}_memory.log".format(worker_n))
     memory_profiler = MemoryProfiler(pid=-1, log_path=mem_log)
@@ -59,6 +60,7 @@ def run_worker(env_id, preprocess_wrapper, seed, worker_n, n_steps_to_run, ckpt_
     print("Cluster established!")
     updates = 0
     steps = 0
+    ckpt_timer.reset()
     while steps < n_steps_to_run:
         start_time = time.time()
 
@@ -72,9 +74,10 @@ def run_worker(env_id, preprocess_wrapper, seed, worker_n, n_steps_to_run, ckpt_
 
         if done:
             w.reset_env()
-        if worker_n == 0 and updates % ckpt_freq == 0:
+        if worker_n == 0 and ckpt_timer.done():
             saver.save(sess, checkpoint_file)
             print("Checkpoint saved to '{}'".format(checkpoint_file))
+            ckpt_timer.reset()
 
     memory_profiler.stop()
 
@@ -83,7 +86,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("env_id")
 parser.add_argument("--n_steps", type=int, default=10)
 parser.add_argument("--n_workers", type=int, default=16)
-parser.add_argument("--ckpt_freq", type=int, default=5)
+parser.add_argument("--ckpt_interval_seconds", type=int, default=60)
 parser.add_argument("--load_ckpt")
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--render", action='store_true')
@@ -116,6 +119,8 @@ if args.preprocessing == 'generic':
 elif args.preprocessing == 'pong':
     preprocess_wrapper = preprocessing.pong_preprocess
 
+ckpt_timer = Timer(duration_seconds=args.ckpt_interval_seconds)
+
 cluster_dict = {}
 ports = get_port_range(start_port=2200, n_ports=args.n_workers)
 cluster_dict["worker"] = ["localhost:{}".format(port)
@@ -130,7 +135,7 @@ def start_worker_process(worker_n):
                seed=args.seed,
                worker_n=worker_n,
                n_steps_to_run=args.n_steps,
-               ckpt_freq=args.ckpt_freq,
+               ckpt_timer=ckpt_timer,
                load_ckpt_file=args.load_ckpt,
                render=args.render,
                log_dir=log_dir)
