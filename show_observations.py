41c4c413a6b755c89e1f389446c8dd5cd3d94daf
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-25 22:10:36 +0200

    Cope with returns

--- show_observations.py
+++ show_observations.py
@@ -9,6 +9,16 @@ Add a line like this to your graph:
                    summarize=2147483647)  # max int32
 
 Then run this script on the output of your run to view each observation saved.
+
+In this code:
+
+        x = tf.Print(x, [graph_s],
+                     message='Observations: ',
+                     summarize=2147483647)  # max int32
+
+        advantage = tf.Print(advantage, [graph_r],
+                     message='Returns: ',
+                     summarize=2147483647)  # max int32
 """
 
 import argparse
@@ -20,19 +30,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('log_file', type=argparse.FileType('r'))
 args = parser.parse_args()
 
-for line in args.log_file:
-    prefix = 'Observations: '
-    if not line.startswith(prefix):
-        continue
-    line = line[len(prefix):]
 
+def parse_array(line):
     # Massage into a form that we can eval()
     line = re.sub('([0-9]) ', '\\1, ', line)
     line = re.sub('\]', '], ', line)
 
-    obs = np.array(eval(line)[0])
-    print("Found observation with shape", obs.shape)
+    array = np.array(eval(line)[0])
+    print("Found array with shape", array.shape)
 
+    return array
+
+
+def show_observations(array):
+    obs = array
     if obs.shape == (1, 80, 80, 4) or obs.shape == (1, 84, 84, 4):
         obs = obs[0]
         obs = np.moveaxis(obs, 2, 0)
@@ -45,7 +56,25 @@ for line in args.log_file:
         obs = np.hstack(obs)
     else:
         print("Unsure how to deal with shape; skipping")
-        continue
-
+        return
     imshow(obs, cmap='gray')
     show()
+
+
+def show_returns(array):
+    returns = array
+    plot(returns)
+    ylabel("Return")
+    xlabel("Step")
+    show()
+
+
+for line in args.log_file:
+    prefix = 'Observations: '
+    if line.startswith(prefix):
+        line = line[len(prefix):]
+        show_observations(parse_array(line))
+    prefix = 'Returns: '
+    if line.startswith(prefix):
+        line = line[len(prefix):]
+        show_returns(parse_array(line))
