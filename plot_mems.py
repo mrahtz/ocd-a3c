6ba976c800fb8e85740f833f2515c9c56c22cc02
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-04-26 20:26:12 +0200

    Add plot_mems.py

---
+++ plot_mems.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Plot process memory usage graphs recorded by utils.profile_memory.
+"""
+
+import argparse
+from glob import glob
+from os.path import join
+
+from pylab import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('mem_log', nargs='*')
+args = parser.parse_args()
+
+for i, log in enumerate(args.mem_log):
+    with open(log) as f:
+        lines = f.read().rstrip().split('\n')
+    mems = [float(l.split()[1]) for l in lines]
+    times = [float(l.split()[2]) for l in lines]
+    rtimes = [t - times[0] for t in times]
+    subplot(len(args.mem_log), 1, i + 1)
+    title(log)
+    plot(rtimes, mems)
+
+tight_layout()
+show()
