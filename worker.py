459e48a2ef2ff4fd1c300ca09775b63d038dd25c
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-05-02 09:51:09 +0200

    Add TODO for commenting no-ops

--- worker.py
+++ worker.py
@@ -117,6 +117,7 @@ class Worker:
     def reset_env(self):
         self.last_o.clear()
         self.last_o.append(self.env.reset())
+        # TODO: comment no-ops
         n_noops = np.random.randint(low=0, high=N_MAX_NOOPS + 1)
         print("%d no-ops..." % n_noops)
         for i in range(n_noops):
