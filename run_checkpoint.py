f730fca51330dfe3a6be5fe6b724901bc54f6a6b
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-06-02 14:20:01 +0200

    Clean up run_checkpoint.py

--- run_checkpoint.py
+++ run_checkpoint.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Run a trained agent.
+Run a trained agent from a checkpoint.
 """
 
 import argparse
@@ -11,7 +11,7 @@ import gym
 import numpy as np
 import tensorflow as tf
 
-from network import Network
+from network import make_inference_network
 from preprocessing import generic_preprocess
 
 
@@ -19,37 +19,9 @@ def main():
     args = parse_args()
     env = gym.make(args.env_id)
     env = generic_preprocess(env, max_n_noops=0)
-    sess, network = get_network(args.ckpt_dir, env.action_space.n)
-    run_agent(env, sess, network)
-
-
-def run_agent(env, sess, network):
-    while True:
-        obs = env.reset()
-        episode_reward = 0
-        done = False
-        while not done:
-            s = np.moveaxis(obs, 0, -1)
-            feed_dict = {network.s: [s]}
-            action_probs = sess.run(network.a_softmax, feed_dict)[0]
-            action = np.random.choice(env.action_space.n, p=action_probs)
-            obs, reward, done, _ = env.step(action)
-            episode_reward += reward
-            env.render()
-            time.sleep(1/60.0)
-        print("Episode reward:", episode_reward)
-
-
-def get_network(ckpt_dir, n_actions):
-    sess = tf.Session()
-    network = Network(scope='global', n_actions=n_actions)
-    ckpt_file = tf.train.latest_checkpoint(ckpt_dir)
-    if not ckpt_file:
-        raise Exception("Couldn't find checkpoint in '{}'".format(ckpt_dir))
-    print("Loading checkpoint from '{}'".format(ckpt_file))
-    saver = tf.train.Saver()
-    saver.restore(sess, ckpt_file)
-    return sess, network
+    sess, obs_placeholder, action_probs_op = \
+        get_network(args.ckpt_dir, env.action_space.n)
+    run_agent(env, sess, obs_placeholder, action_probs_op)
 
 
 def parse_args():
@@ -58,6 +30,40 @@ def parse_args():
     parser.add_argument("ckpt_dir")
     args = parser.parse_args()
     return args
+
+
+def get_network(ckpt_dir, n_actions):
+    sess = tf.Session()
+
+    with tf.variable_scope('global'):
+        obs_placeholder, _, action_probs_op, _, _ = \
+            make_inference_network(n_actions, weight_inits='glorot', debug=False)
+
+    ckpt_file = tf.train.latest_checkpoint(ckpt_dir)
+    if not ckpt_file:
+        raise Exception("Couldn't find checkpoint in '{}'".format(ckpt_dir))
+    print("Loading checkpoint from '{}'".format(ckpt_file))
+    saver = tf.train.Saver()
+    saver.restore(sess, ckpt_file)
+
+    return sess, obs_placeholder, action_probs_op
+
+
+def run_agent(env, sess, obs_placeholder, action_probs_op):
+    while True:
+        obs = env.reset()
+        episode_reward = 0
+        done = False
+        while not done:
+            s = np.moveaxis(obs, 0, -1)
+            feed_dict = {obs_placeholder: [s]}
+            action_probs = sess.run(action_probs_op, feed_dict)[0]
+            action = np.random.choice(env.action_space.n, p=action_probs)
+            obs, reward, done, _ = env.step(action)
+            episode_reward += reward
+            env.render()
+            time.sleep(1 / 60.0)
+        print("Episode reward:", episode_reward)
 
 
 if __name__ == '__main__':
