94ebf172b8864a7b3161937b8d0504e14bfb4a30
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-07-09 20:56:35 +0200

    Monitor before apply preprocessing wrappers

--- train.py
+++ train.py
@@ -133,15 +133,16 @@ def make_envs(env_id, preprocess_wrapper, max_n_noops, n_envs, seed, debug,
             # global seed completely changes the whole set of env seeds.
             env_seed = seed * n_envs + env_n
             env.seed(env_seed)
-            if debug:
-                env = NumberFrames(env)
-            env = preprocess_wrapper(env, max_n_noops)
 
             if env_n == 0:
                 env_log_dir = osp.join(log_dir, "env_{}".format(env_n))
             else:
                 env_log_dir = None
             env = MonitorEnv(env, "Env {}".format(env_n), log_dir=env_log_dir)
+
+            if debug:
+                env = NumberFrames(env)
+            env = preprocess_wrapper(env, max_n_noops)
 
             return env
 
